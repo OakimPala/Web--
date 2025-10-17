c02fc5b2e4e14e7874f7349114bb31a3a28320b9
OakimPala <125463694+OakimPala@users.noreply.github.com>
2025-10-18 00:18:24 +0300

    Add files via upload

---
+++ dishes.js
@@ -0,0 +1,96 @@
+// Массив всех блюд
+const dishes = [
+  // 🥣 Супы
+  {
+    keyword: "borsch",
+    name: "Борщ",
+    price: 109,
+    category: "soup",
+    count: "300 мл",
+    image: "https://prostokvashino.ru/upload/resize_cache/iblock/5e1/800_800_0/5e17b58e5a12db3624b168644414b70d.jpg"
+  },
+  {
+    keyword: "solyanka",
+    name: "Солянка",
+    price: 149,
+    category: "soup",
+    count: "300 мл",
+    image: "https://s.myspar.ru/upload/img/10/1000/100071295.jpg?1721035848"
+  },
+  {
+    keyword: "harcho",
+    name: "Харчо",
+    price: 149,
+    category: "soup",
+    count: "300 мл",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzBjI7ew6jgRB9mTzv4Icxq-ePMN6bCPfb6Q&s"
+  },
+  {
+    keyword: "shurpa",
+    name: "Шурпа",
+    price: 149,
+    category: "soup",
+    count: "300 мл",
+    image: "https://anor24.ru/wp-content/uploads/2020/06/ANOR3261.jpg"
+  },
+
+  // 🍛 Основные блюда
+  {
+    keyword: "plov",
+    name: "Плов",
+    price: 150,
+    category: "main",
+    count: "300 гр",
+    image: "https://img09.rl0.ru/afisha/e1200x1200i/daily.afisha.ru/uploads/images/9/29/929a072cc910f5b436b4608e8f25530e.jpg"
+  },
+  {
+    keyword: "kebabs",
+    name: "Казан кебаб",
+    price: 240,
+    category: "main",
+    count: "300 гр",
+    image: "https://caravellacatering.com/wp-content/uploads/2023/10/kazan-kebab.jpg"
+  },
+  {
+    keyword: "cutlets",
+    name: "Котлеты с пюре",
+    price: 240,
+    category: "main",
+    count: "300 гр",
+    image: "https://club.mysamson.ru/upload/resize_cache/webp/resize_cache/iblock/70d/480_480_2/zdk6s5bokxrxbxtbtebr7gh85dbvpq6t.webp"
+  },
+  {
+    keyword: "free",
+    name: "Жаренная картошка с грибами",
+    price: 240,
+    category: "main",
+    count: "300 гр",
+    image: "https://menunedeli.ru/wp-content/uploads/2015/05/45/zharenaja-kartoshka-s-gribami-i-lukom-%D0%BE_opt.jpg"
+  },
+
+  // 🍹 Напитки
+  {
+    keyword: "orange",
+    name: "Апельсиновый сок",
+    price: 120,
+    category: "drink",
+    count: "300 мл",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRE2c96ckaH2aSZm89Jxenx-RKTVeXg8TOkhw&s"
+  },
+  {
+    keyword: "apple",
+    name: "Яблочный сок",
+    price: 90,
+    category: "drink",
+    count: "300 мл",
+    image: "https://eda.ru/images/RecipePhoto/930x622/domashniy-yablochnyy-sok_173985_photo_181327.webp"
+  },
+  {
+    keyword: "cherry",
+    name: "Вишнёвый сок",
+    price: 110,
+    category: "drink",
+    count: "300 мл",
+    image: "https://m.dom-eda.com/uploads/topics/preview/00/00/09/92/8ee3f425c3_1000.jpg"
+  }
+];
